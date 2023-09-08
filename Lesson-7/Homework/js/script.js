1e87a94ad8008389f6b9dcd0ba20d36814f76047
Anna Zykova <137221931+fyzaanya@users.noreply.github.com>
2023-09-09 00:09:00 +0300

    Update script.js

--- Lesson-7/Homework/js/script.js
+++ Lesson-7/Homework/js/script.js
@@ -1,31 +1,31 @@
 // Створіть функцію-конструктор Calculator, який створює об’єкти з трьома методами:
 
-function Calculator() {
+// function Calculator() {
 
 // read() запитує два значення за допомогою prompt і запам’ятовує їх у властивостях об’єкта.
     
-    this.read = function () {
-        this.firstNumber = parseFloat(prompt('Введіть перше число: '));
-        this.secondNumber = parseFloat(prompt('Введіть друге число: '));
-    };
+    // this.read = function () {
+    //     this.firstNumber = parseFloat(prompt('Введіть перше число: '));
+    //     this.secondNumber = parseFloat(prompt('Введіть друге число: '));
+    // };
 
 // sum() повертає суму цих властивостей.
 
-    this.sum = function () {
-        return this.firstNumber + this.secondNumber;
-    };
+    // this.sum = function () {
+    //     return this.firstNumber + this.secondNumber;
+    // };
 
 // mul() повертає результат множення даних властивостей.
 
-    this.mul = function () {
-        return this.firstNumber * this.secondNumber;
-    };
-}
+//     this.mul = function () {
+//         return this.firstNumber * this.secondNumber;
+//     };
+// }
 
-var calculator = new Calculator();
-calculator.read();
+// var calculator = new Calculator();
+// calculator.read();
 
-console.group("Калькулятор:");
-console.log("Сума чисел = ", calculator.sum());
-console.log("Добуток чисел = ", calculator.mul());
-console.groupEnd();
+// console.group("Калькулятор:");
+// console.log("Сума чисел = ", calculator.sum());
+// console.log("Добуток чисел = ", calculator.mul());
+// console.groupEnd();
